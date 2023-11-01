f4e3b35efa74511ab72a80a8f17d2c0ea44b3ed0
Gabriel <gabriel.amaro97@hotmail.com>
2023-11-01 12:53:24 -0300

    actualización de ecommerce, funcional

---
+++ client/js/products.js
@@ -0,0 +1,70 @@
+//lista de productos
+const productos = [
+  {
+    id: "1",
+    productName: "Aceite BiOlive 500ml",
+    price: 2.99,
+    quanty: 1,
+    img: "./media/botella3.jpg",
+  },
+  {
+    id: "2",
+    productName: "Aceite Laur 500ml",
+    price: 6.99,
+    quanty: 1,
+    img: "./media/botella3.jpg",
+  },
+  {
+    id: "3",
+    productName: "Aceite Wangler 500ml",
+    price: 8.55,
+    quanty: 1,
+    img: "./media/botella.jpg",
+    category: "botella3",
+  },
+
+  {
+    id: "4",
+    productName: "Aceite Wangler 500ml",
+    price: 8.55,
+    quanty: 1,
+    img: "./media/botella3.jpg",
+    category: "botella1,"
+  },
+
+  {
+    id: "5",
+    productName: "Aceite Wangler 500ml",
+    price: 8.55,
+    quanty: 1,
+    img: "./media/botella.jpg",
+    category: "botella3",
+  },
+
+  {
+    id: "6",
+    productName: "Aceite Wangler 500ml",
+    price: 8.55,
+    quanty: 1,
+    img: "./media/botella3.jpg",
+    category: "botella3",
+  },
+  {
+    id: "7",
+    productName: "Aceite Wangler 500ml",
+    price: 8.55,
+    quanty: 1,
+    img: "./media/botella.jpg",
+    category: "botella2",
+  },
+  {
+    id: "8",
+    productName: "Aceite Wangler 500ml",
+    price: 8.55,
+    quanty: 1,
+    img: "./media/botella3.jpg",
+    category: "botella1",
+  },
+
+];
+
